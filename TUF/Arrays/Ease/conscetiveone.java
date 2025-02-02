957739ab83fdc1f06c05eb6950feae96e5a0d658
rahulYUV <rs6205903996@gmail.com>
2025-02-02 12:48:26 +0530

    MaxiSteak

---
+++ TUF/Arrays/Ease/conscetiveone.java
@@ -0,0 +1,29 @@
+package TUF.Arrays.Ease;
+import java.util.*;
+
+public class conscetiveone {
+    public static void main(String[] args) {
+        int[] arr={1,1,1,1,0,0,1,1,1,0,1,1,1,1,1,0};
+        int MaximumStreak = maxiStreak(arr);
+        System.out.println(MaximumStreak);
+
+    }
+    static int maxiStreak(int []arr){
+        int n = arr.length;
+        int maxi =0;
+        int count=0;
+        for(int i =0;i<n;i++){
+            if(arr[i]==1){
+                count++;
+            }
+            else{
+                count=0;
+
+            }
+            maxi= Math.max(maxi,count);
+
+        }
+        return maxi;
+    }
+    
+}
